a393bc811ef249ce37c214c93bf01d7861ab8433
MrWhiteShapi <aigunovshapi@yandex.ru>
2024-06-08 14:23:16 +0300

    Test codestyle refactor once again

--- src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorTest.java
@@ -7,8 +7,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
